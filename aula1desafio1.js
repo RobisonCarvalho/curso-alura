a7e671cf5ea772c8fe822725dd054a1292956a67
RobisonCarvalho <robison.carvalho12@gmail.com>
2023-11-15 16:10:34 +0000

    1º desafio aula 1

---
+++ aula1desafio1.js
@@ -0,0 +1,15 @@
+var nome = "Robison Carvalho "
+var real = 100.00
+var cambioDolar = 4.86;
+var cambioEuro = 5.27;
+var cambioBath = 0.14;
+var cambioLibra = 6.05;
+var valorEmRealDolar = real / cambioDolar;
+var valorEmRealEuro = real / cambioEuro;
+var valorEmRealBath = real / cambioBath;
+var valorEmRealLibra = real / cambioLibra;
+alert("Olá Sr. " + nome +
+"\n$" + valorEmRealDolar.toFixed(2) +
+"\n$ " + valorEmRealEuro.toFixed(2) +
+"\n$ " + valorEmRealBath.toFixed(2) +
+"\n$ " + valorEmRealLibra.toFixed(2));
