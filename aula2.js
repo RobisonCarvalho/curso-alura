3ad41163b5bfaaf833d363f9f198f07624c269cc
RobisonCarvalho <robison.carvalho12@gmail.com>
2023-11-17 16:27:12 +0000

    Aula 2 - conf. aleatorio e repetição Alura

--- aula2.js
+++ aula2.js
@@ -1,11 +1,14 @@
-var numeroSecreto = 581
+var numeroSecreto = parseInt(Math.random()* 1001);
 
-var chute = prompt('Digite o numero entre 1 e 1000');
+//Desafio: configura para numero de chances, colocar um limete de chances e coloca numero aleatorio de 1 à 1000
+while(chute != numeroSecreto) {
+    var chute = prompt('Digite o numero entre 0 e 1000');
 
-if (chute == numeroSecreto){
-    alert('Acertou')
-} else if (chute > numeroSecreto){
-    alert('Está quase la! mas o numero é menor')
-} else if (chute < numeroSecreto){
-    alert('Está quase la! mas o numero é maior')
+    if (chute == numeroSecreto){
+        alert('Acertou')
+    } else if (chute > numeroSecreto){
+        alert('Está quase la! mas o numero é menor')
+    } else if (chute < numeroSecreto){
+        alert('Está quase la! mas o numero é maior')
+  }
 }
